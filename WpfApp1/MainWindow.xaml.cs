85ec759ac7b2f6b7b2bc2b619ee1b3164340d079
Иван Александрович Дрей <83556084+dreyvania999@users.noreply.github.com>
2022-10-26 12:12:17 +0300

    стр

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Classes;
+using WpfApp1.Pages;
 
 namespace WpfApp1
 {
@@ -23,6 +26,52 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            FrameClass.MainFrame = fMain;
+            FrameClass.MainFrame.Navigate(new ActivatedPage());
         }
+
+        
+private bool PasswordCheck(PasswordBox PB)
+        {
+            PB.ToolTip = "мпа";
+            Regex presenceALL = new Regex(@"^(?=.*[a-z]){3,}(?=.*[A-Z])(?=.*\d){2,}(?=.*[@$!#№%*?&=+_-])[0-9A-Za-z@$!%*?&=+_-]{8,}$");
+            Regex presenceNum = new Regex(@"^.*\d.*\d$");
+            Regex presenceLowerCase = new Regex(@"^.*[a-z].*[a-z].*[a-z].*$");
+            Regex presenceHieghtCase = new Regex(@"^.*[A-Z].*$");
+            Regex presenceSize = new Regex(@"^.{8,}$");
+            Regex presenceSymbol = new Regex(@"^.*[@$!#№%*?&=+_-].*$");
+            if (presenceALL.IsMatch(PB.Password.ToString()))
+            {
+                PB.ToolTip = (" presenceALL ");
+            }
+            else
+            {
+                if (!presenceNum.IsMatch(PB.Password.ToString()))
+                {
+                    PB.ToolTip += (" presenceNum ");
+                }
+                if (!presenceSize.IsMatch(PB.Password.ToString()))
+                {
+                    PB.ToolTip += (" presenceSize ");
+                }
+                if (!presenceLowerCase.IsMatch(PB.Password.ToString()))
+                {
+                    PB.ToolTip += (" presenceLowercase ");
+                }
+                if (!presenceHieghtCase.IsMatch(PB.Password.ToString()))
+                {
+                    PB.ToolTip += (" presenceHieghtCase ");
+                }
+                if (!presenceSymbol.IsMatch(PB.Password.ToString()))
+                {
+                    PB.ToolTip += (" presenceSymbol ");
+                }
+                return false;
+            }
+            
+            return true;
+        }
+
+       
     }
 }
---
+++ WpfApp1/Pages/ActivatedPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Classes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class ActivatedPage : Page
+    {
+        public ActivatedPage()
+        {
+            InitializeComponent();
+        }
+        private void btnAuto_Click(object sender, RoutedEventArgs e)
+        {
+            FrameClass.MainFrame.Navigate(new AutarizationPage());
+        }
+
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            FrameClass.MainFrame.Navigate(new RegistrationPage());
+        }
+    }
+}
